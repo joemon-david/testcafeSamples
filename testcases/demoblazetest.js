b1bca684ac02d507ea72cdb51af12188bc124544
Joemon David <joemon.david@mobileheartbeat.com>
2021-01-23 20:28:46 +0530

    Commiting the script code

--- testcases/demoblazetest.js
+++ testcases/demoblazetest.js
@@ -1,20 +1,14 @@
-{
-  "name": "testcafeneo",
-  "version": "1.0.0",
-  "description": "\"To create an end to end automation framework using js technologies\"",
-  "main": "index.js",
-  "scripts": {
-    "test": "testcafe chrome ./testcases"
-  },
-  "keywords": [
-    "e2e",
-    "javascript",
-    "web",
-    "automation"
-  ],
-  "author": "Joemon David",
-  "license": "ISC",
-  "dependencies": {
-    "testcafe": "^1.10.1"
-  }
-}
+
+import {Selector} from 'testcafe';
+
+fixture `Getting started with Demoblaze application`
+.page `https://www.demoblaze.com/index.html`;
+
+test('Login to DemoBlaze app', async t => {
+
+    await t
+    .click('#login2')
+    .typeText('#loginusername','joemon')
+    .typeText('#loginpassword','joemon')
+    .click('[onclick="logIn()"]')
+});
